c7e936c0103650097a8cf4b1ee95899edadf5311
RAUNAK BANJARI <88834316+raunakbanjari@users.noreply.github.com>
2022-07-26 18:38:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 stats = "";
 video = "";
+objects = [];
 
 function preload(){
     video = createVideo("video.mp4");
@@ -18,6 +19,23 @@ function start(){
 function draw(){
     image(video , 0 , 0 , 480 , 380);
 
+    if(stats != ""){
+        objectDetector.detect(video , got_results);
+        
+
+        for(i = 0 ; i < objects.length ; i++){
+            console.log(objects);
+            document.getElementById("no_of_objects").innerHTML = "Number of Objects Detected : " + objects.length;
+        document.getElementById("status").innerHTML = "Objects Detected!"
+        percent = floor(objects[i].confidence * 100);
+        fill('red');
+        text(objects[i].label + " " + percent + "%" , objects[i].x , objects[i].y);
+        stroke('red');
+        noFill();
+        rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
+
 }
 function modelLoaded(){
     console.log("model loaded");
@@ -25,4 +43,17 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function stop_video(){
+    video.stop();
+}
+function pause(){
+    video.pause();
+}
+function got_results(error , results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
 }
